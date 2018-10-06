d109a00d4405b1afe74d4c3098a2d0742c76ee77
Stuart Herbert <stuart@stuartherbert.com>
2018-10-06 11:36:44 +0100

    Refactor: HasMethod is now called HasMethodCalled

--- src/V1/PhpReflection/HasMethodCalled.php
+++ src/V1/PhpReflection/HasMethodCalled.php
@@ -34,7 +34,7 @@ use GanbaroDigital\MissingBits\ClassesAndObjects\StatelessClass;
 /**
  * does the context contain a named method?
  */
-class HasMethod
+class HasMethodCalled
 {
     // we don't want you making objects from this class, sorry!
     use StatelessClass;
