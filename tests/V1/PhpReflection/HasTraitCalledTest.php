7e7152ecd736a119561a620d10f6c1793ef5a9d0
Stuart Herbert <stuart@stuartherbert.com>
2018-10-06 12:58:30 +0100

    Tests: added unit tests for HasTraitCalled

---
+++ tests/V1/PhpReflection/HasTraitCalledTest.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * Copyright (c) 2017-present Ganbaro Digital Ltd
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If you wish to use this program in proprietary software, you can purchase
+ * a closed-source license. Contact licensing@ganbarodigital.com for details.
+ *
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2017-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   https://www.gnu.org/licenses/agpl.html  GNU Affero GPL v3
+ * @link      http://ganbarodigital.github.io/php-mv-deep-reflection
+ */
+
+namespace GanbaroDigitalTest\DeepReflection\V1\PhpReflection;
+
+use GanbaroDigital\DeepReflection\V1\PhpContexts;
+use GanbaroDigital\DeepReflection\V1\PhpReflection;
+use GanbaroDigital\DeepReflection\V1\PhpReflection\HasTraitCalled;
+use GanbaroDigital\MissingBits\Checks\Check;
+use GanbaroDigital\MissingBits\ErrorResponders\OnFatal;
+use GanbaroDigitalTest\DeepReflection\V1\PhpFixtures\AddTraitsToContainer;
+use PhpUnit\Framework\TestCase;
+
+require_once(__DIR__ . '/../PhpFixtures/AddTraitsToContainer.php');
+
+/**
+ * @coversDefaultClass GanbaroDigital\DeepReflection\V1\PhpReflection\HasTraitCalled
+ */
+class HasTraitCalledTest extends TestCase
+{
+    use AddTraitsToContainer;
+
+    /**
+     * @covers ::__construct
+     * @expectedException Error
+     */
+    public function test_cannot_be_instantiated()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $context = new PhpContexts\PhpGlobalContext;
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $unit = new HasTraitCalled('FooTrait', $context);
+
+        // ----------------------------------------------------------------
+        // test the results
+    }
+
+    /**
+     * @covers ::check
+     */
+    public function test_returns_true_if_named_trait_in_context()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $context = new PhpContexts\PhpGlobalContext;
+        $this->addMinimalTraits($context);
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertTrue(HasTraitCalled::check('FooTrait', $context));
+    }
+
+    /**
+     * @covers ::check
+     */
+    public function test_returns_false_if_named_trait_not_in_context()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $context = new PhpContexts\PhpGlobalContext;
+        $this->addMinimalTraits($context);
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertFalse(HasTraitCalled::check('not_a_class', $context));
+    }
+
+}
