1081678dc27f691b94e4bbcf225d335645e3c609
Stuart Herbert <stuart@stuartherbert.com>
2018-10-06 14:58:00 +0100

    New: added GetAllParameters

---
+++ tests/V1/PhpFixtures/BasicGlobalExamples/FooClassWithMethodsWithParams.php
@@ -0,0 +1,14 @@
+<?php
+
+class FooClassWithMethodsWithParams
+{
+    public function methodWithNoParams()
+    {
+        // do nothing
+    }
+
+    public function methodWithFourParams($first, string $second, array $third, int $fourth = null)
+    {
+        // do nothing
+    }
+}
